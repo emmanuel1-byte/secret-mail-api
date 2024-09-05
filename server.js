e484a5be6a364524d721314c6916c7918c8183b5
emmanuel1-byte <emmanuelhilary9@gmail.com>
2024-09-05 01:34:11 +0100

    added api documentions

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import { createServer } from "http";
 import express from "express";
 import logger from "./src/utils/logger.js";
+import cors from "cors";
+import YAML from "yaml";
+import fs from "fs";
+import swaggerUi from "swagger-ui-express";
 import {
   globalErrorhandler,
   routeNotFoundHandler,
@@ -15,11 +19,17 @@ const server = createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors({ origin: "*", methods: ["POST", "GET", "DELETE"] }));
+
+const file = fs.readFileSync("doc/swagger.yaml", "utf-8");
+const swaggerDocument = YAML.parse(file);
+
 app.get("/", (req, res) => {
   return respond(res, 200, "API is healthy and running...");
 });
 
 app.use("/api/v1/disposable-email", email);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(globalErrorhandler);
 app.use(routeNotFoundHandler);
